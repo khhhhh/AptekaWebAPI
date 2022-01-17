4a3a5c11b4a36492f428aea94943452e27f96a5c
Taras <taras.iskiv@gmail.com>
2022-01-17 23:17:27 +0100

    Added templates of functions to Controller.

---
+++ AptekaWebAPI/Controllers/Pharmacy.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AptekaWebAPI.Controllers
+{
+    [Route("/api/pharmacy")]
+    public class Pharmacy
+    {
+        [HttpGet("/all")]
+        public ActionResult GetAll()
+        {
+            return null;
+        }
+
+        [HttpGet("/{id}")]
+        public ActionResult GetById([FromRoute] int id)
+        {
+            return null;
+        }
+
+        [HttpGet("/{name}")]
+        public ActionResult GetByName([FromRoute] string name)
+        {
+            return null;
+        }
+
+
+        [HttpPut("/{id}")]
+        public ActionResult Modify([FromRoute] int id)
+        {
+            return null;
+        }
+
+        [HttpDelete("/{id}")]
+        public ActionResult Delete([FromRoute] int id)
+        {
+            return null;
+        }
+
+        //add to cart
+
+        //create new controller for carts
+    }
+}
