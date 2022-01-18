6cb633306687d4d5c483f5151ded9c5f931a7291
Taras <taras.iskiv@gmail.com>
2022-01-18 19:56:14 +0100

    Added empty functions to controllers.

---
+++ AptekaWebAPI/Controllers/Cart.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AptekaWebAPI.Controllers
+{
+    [ApiController]
+    [Route("/user/api/cart")]
+    public class Cart : ControllerBase
+    {
+        [HttpPost("{id}")]
+        public ActionResult AddById([FromRoute] int id)
+        {
+            return NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult RemoveById([FromRoute] int id)
+        {
+            return NotFound();
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Modify([FromRoute] int id)
+        {
+            return NotFound();
+        }
+
+        [HttpGet("/all")]
+        public ActionResult GetAll()
+        {
+            return NotFound();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetByID([FromRoute] int id)
+        {
+            return NotFound();
+        }
+    }
+}
