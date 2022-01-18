4b7795c4cbd725af214a1637c384b43b18ace562
Taras <taras.iskiv@gmail.com>
2022-01-18 20:39:44 +0100

    Added generateToken method in class Token

---
+++ AptekaWebAPI/Token/Token.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AptekaWebAPI.Token
+{
+    public class Token
+    {
+        public static List<string> Tokens;
+
+        public string generateNewToken()
+        {
+            var token = Guid.NewGuid().ToString();
+            Tokens.Add(token);
+            return token;
+        }
+
+        ~Token()
+        {
+            Tokens.Clear();
+        }
+    }
+}
