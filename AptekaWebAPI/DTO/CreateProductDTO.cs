ecb76fdc8058a16a75cce4744b31bbd5398352a6
aleh <oleg.hutsko@gmail.com>
2022-01-28 17:02:30 +0100

    Add product DTOs

---
+++ AptekaWebAPI/DTO/CreateProductDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AptekaWebAPI.Properties.DTOs
+{
+    public class CreateProductDto
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
---
+++ AptekaWebAPI/DTO/UpdateProductDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AptekaWebAPI.Properties.DTOs
+{
+    public class UpdateProductDto
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
