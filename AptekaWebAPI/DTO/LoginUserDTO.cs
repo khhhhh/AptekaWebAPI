a97c31828e785bb3e4ea1d8ebc0f49352965c89e
aleh <oleg.hutsko@gmail.com>
2022-01-29 15:25:16 +0100

    Add new DTOs.

---
+++ AptekaWebAPI/DTO/LoginUserDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AptekaWebAPI.DTO
+{
+    public class LoginUserDTO
+    {
+        [Required]
+        [EmailAddress]
+        public string Login { get; set; }
+
+        [Required]
+        [MinLength(8)]
+        [MaxLength(25)]
+        public string Password { get; set; }
+    }
+}
---
+++ AptekaWebAPI/DTO/UpdateUserDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AptekaWebAPI.DTO
+{
+    public class UpdateUserDTO
+    {
+
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+        [Required]
+        public string CurrentPassword { get; set; }
+
+        public string NewPassword { get; set; }
+    }
+}
