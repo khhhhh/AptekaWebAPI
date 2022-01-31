724243e689520ef4c6dc10cf9d005279239e11a7
Taras <taras.iskiv@gmail.com>
2022-01-31 22:47:02 +0100

    Added controller buy

---
+++ AptekaWebAPI/Controllers/Buy.cs
@@ -0,0 +1,24 @@
+﻿using AptekaWebAPI.Tokens;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AptekaWebAPI.Controllers
+{
+
+    [ApiController]
+    [Route("api/buy")]
+    public class Buy : ControllerBase
+    {
+        [HttpGet()]
+        public ActionResult BuyFromCart([FromHeader] string token)
+        {
+            var activeUser = new ActiveUsers().GetAllLoginedUsers().Where(x => x.Token == token).FirstOrDefault();
+            if (activeUser == null) throw new Exception();
+
+            return Ok();
+        }
+    }
+}
