9f70ffd9f8782d619b928c9a7627866b2d7bf04a
aleh <oleg.hutsko@gmail.com>
2022-02-01 11:56:14 +0100

    extend seeder.

--- AptekaWebAPI/Entities/PharamcySeeder.cs
+++ AptekaWebAPI/Entities/PharamcySeeder.cs
@@ -9,6 +9,10 @@ namespace AptekaWebAPI.Entities
     public class PharamcySeeder
     {
         private readonly PharmacyContext pharmacyDB;
+        private List<ProductCategory> productCategories;
+        private List<Product> products;
+        private List<Address> addresses;
+        private List<User> users;
         public PharamcySeeder(PharmacyContext pharmacyDB)
         {
             this.pharmacyDB = pharmacyDB;
@@ -26,27 +30,102 @@ namespace AptekaWebAPI.Entities
             return result.ToString();
         }
 
+        private IEnumerable<ProductCategory> GetProductCategories()
+        {
+            productCategories = new List<ProductCategory>()
+            {
+                new ProductCategory()
+                {
+                    Name = "Analgesic"
+                },
+                new ProductCategory()
+                {
+                    Name = "Antipyretics"
+                },
+                new ProductCategory()
+                {
+                    Name = "Antihistamines"
+                },
+            };
+            return productCategories; 
+        }
+
+        private IEnumerable<Product> GetProducts()
+        {
+            products = new List<Product>()
+            {
+                new Product()
+                {
+                    Name = "Aspirin",
+                    Categories = new List<ProductCategory> { productCategories[0], productCategories[1] },
+                    Price = 10,
+                    Count = 100,
+                    Description = "Perfect against your headache!"
+                },
+                new Product()
+                {
+                    Name = "Chlorphenamine",
+                    Categories = new List<ProductCategory> { productCategories[0], productCategories[1] },
+                    Price = 15,
+                    Count = 200,
+                    Description = "Helps you stop sneezing!"
+                }
+            };
+
+            return products;
+        }
+        private IEnumerable<Address> GetAddresses()
+        {
+            addresses = new List<Address>()
+            {
+                new Address()
+                {
+                    City = "Lublin",
+                    Street = "ul. Langiewicza, 5",
+                    PostalCode = "20-035",
+                },
+                new Address()
+                {
+                    City = "Warszawa",
+                    Street = "al. Jana Pawła II, 10",
+                    PostalCode = "00-123",
+                },
+                new Address()
+                {
+                    City = "Bydgoszcz",
+                    Street = "ul. Księdza Hugona Kołłątaja, 5",
+                    PostalCode = "85-008",
+                },
+            };
+            return addresses;
+        }
         private IEnumerable<User> GetUsers()
         {
-            var users = new List<User>()
+            users = new List<User>()
             {
                 new User()
                 {
                     Email = "dimon1432b@gmail.com",
                     IsAdmin = true,
-                    PasswordHash = getHash("password")
+                    PasswordHash = getHash("password"),
+                    Address = addresses[1],
+                    Name = "Dzmitry",
                 },
                 new User()
                 {
                     Email = "oleg_hutsko@mail.ru",
                     IsAdmin = true,
-                    PasswordHash = getHash("password123")
+                    PasswordHash = getHash("password123"),
+                    Address = addresses[0],
+                    Name = "Aleh",
                 },
                 new User()
                 {
                     Email = "taras.iskiv@gmail.com",
                     IsAdmin = true,
-                    PasswordHash = getHash("internet")
+                    PasswordHash = getHash("internet"),
+                    Address = addresses[2],
+                    Name = "Taras",
                 }
             };
 
@@ -59,8 +138,15 @@ namespace AptekaWebAPI.Entities
             {
                 if (!pharmacyDB.Users.Any())
                 {
-                    var restaurants = GetUsers();
-                    pharmacyDB.Users.AddRange(restaurants);
+                    var addresses = GetAddresses(); 
+                    var users = GetUsers();
+                    var productCategories = GetProductCategories();
+                    var products = GetProducts();
+
+                    pharmacyDB.Addresses.AddRange(addresses);
+                    pharmacyDB.ProductCategories.AddRange(productCategories);
+                    pharmacyDB.Users.AddRange(users);
+                    pharmacyDB.Products.AddRange(products);
                     
                     pharmacyDB.SaveChanges();
                 }
